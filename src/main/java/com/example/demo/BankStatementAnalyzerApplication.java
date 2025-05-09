82af74b0385ee90281ca33d64541e6c6d9ee3755
Anand Hiremani <anandshiremani123@gmail.com>
2025-05-09 13:03:11 +0530

    My third Commit

--- src/main/java/com/example/demo/BankStatementAnalyzerApplication.java
+++ src/main/java/com/example/demo/BankStatementAnalyzerApplication.java
@@ -8,6 +8,7 @@ public class BankStatementAnalyzerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankStatementAnalyzerApplication.class, args);
+		System.out.println("Anand hiremani");
 	}
 
 }
