ee5e65477fe234f27ca21befd63939edb754a3a7
Anand Hiremani <anandshiremani123@gmail.com>
2025-05-09 13:26:26 +0530

    Untracked Controller directory from Git

--- src/main/java/com/example/demo/BankStatementAnalyzerApplication.java
+++ src/main/java/com/example/demo/BankStatementAnalyzerApplication.java
@@ -9,7 +9,6 @@ public class BankStatementAnalyzerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankStatementAnalyzerApplication.class, args);
 		System.out.println("Anand hiremani");
-		System.out.println("Anand hiremani");
 	}
 
 }
